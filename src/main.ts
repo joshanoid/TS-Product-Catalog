85a06c4b6cb08ddd94b1a32abd5d8c775f19b38f
József Varga <joshanoid@gmail.com>
2025-09-21 18:07:52 +0200

    fix: not setting the store type caused a ts error related to incompatible products type (empty array was treated as never)

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { ProductList } from './features/ProductList/index.ts';
 import { Store } from './store.ts';
+import type { ProductsStore } from './types.ts';
 import './styles.scss';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const store = new Store({ products: [], loading: true, error: null });
+  const store = new Store<ProductsStore>({
+    products: [],
+    loading: true,
+    error: null,
+  });
   const app = new ProductList(document.querySelector('#root')!, store);
 
   app.render();
